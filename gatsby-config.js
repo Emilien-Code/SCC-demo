4fb1fc5ec1be206d97e347e92cb89b9607cab843
Emilien <emilien.ginko@gmail.com>
2022-09-04 18:29:42 +0200

    🔧 test of gatsby-source-sanity

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,21 @@ module.exports = {
     title: `SCC-DEMO`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: ["gatsby-plugin-sass",
+  // {
+  //   resolve: `gatsby-source-sanity`,
+  //   options: {
+  //     projectId: `h3asrjaf`,
+  //     dataset: `production`,
+  //     watchMode:true,
+  //     // a token with read permissions is required
+  //     // if you have a private dataset
+  //     token: "skNyn69iJbBhs0tGiL7Z7VwgWMQJOkGqujd1D5OoN2vNeSYIuFJlBheyysklbvzHQMW55GV6qrOFQxH5kjByFs0vMveUXHodKUGV6altmWygDdQUq187CFIZZ2tiM5hSC7J0ASZJn83TOn92rdWZgiGxTq8opFLwfBKVahgsQAyJHMIOmkDW",
+  //     // If the Sanity GraphQL API was deployed using `--tag <name>`,
+  //     // use `graphqlTag` to specify the tag name. Defaults to `default`.
+  //     graphqlTag: 'default',
+  //     overlayDrafts: true
+  //   },
+  // }
+]
 };
