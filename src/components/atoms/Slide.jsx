3fba7ed2db81dd3d85e8de09e9462ff397638d9c
Emilien <emilien.ginko@gmail.com>
2022-09-06 16:46:33 +0200

    🔧 img by default

--- src/components/atoms/Slide.jsx
+++ src/components/atoms/Slide.jsx
@@ -7,7 +7,7 @@ import defaultImg from '../../assets/images/client-images/Sliders-img/Image.png'
 const Slide = ({data}) => {
     return (
         <div className="slide">
-            {<img src={data.image.asset.publicUrl ? data.image.asset.publicUrl : defaultImg} alt={data.altText} /> }
+            {data.image ? <img src={data.image.asset.publicUrl ? data.image.asset.publicUrl : defaultImg} alt={data.altText} /> : <img src={defaultImg} alt="image par defaut"/> }
             <h1>{data.title}</h1>
             <p>{data.paragraph}</p>
         </div>
