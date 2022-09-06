42e4c48dbeaa99a2ddedabb6e176a307acefc7c8
Emilien <emilien.ginko@gmail.com>
2022-09-06 14:53:03 +0200

    🔧 Sanity connected to front-end

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,123 +11,21 @@ import SliceMachine from "../components/utils/SliceMachine"
 const video = "https://source.unsplash.com/random/1144×606?buildings";
 const client_image = "https://source.unsplash.com/random/1144×606?buildings";
 const  sliderImage  = "https://source.unsplash.com/random/300×300?buildings";
-const workArray = [
-  {
-    type: "header",
-    display: "search"
-  },{
-    type:"Hero",
-    title:"Manage all your company's work at once",
-    primaryParagraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in sollicitudin quam, eu auctor elit. Integer id urna id tortor convallis semper sed sed odio. ", 
-    secondaryParagraph:"Nam sit amet pretium tortor. Praesent sagittis fermentum faucibus. Quisque luctus ex in sapien lacinia ullamcorper.", 
-    display:"img-right",
-    alternativeText:"illustration",
-    btn1:"Join Wait List",
-    btn2:"Request a demo",
-    image: client_image,
-  },{
-    type:"Steps",
-    stepsContent : [{
-      img : "bolt",
-      paragraph : "Nam sit amet pretium tortor. Praesent sagittis fermentum faucibus.",
-      title : "Build your own cloud",
-      link:"/"
-  },{
-      img : "cloud",
-      paragraph : "Nam sit amet pretium tortor. Praesent sagittis fermentum faucibus.",
-      title : "Protect your work",
-      link:"/"
-  },{
-      img : "lock",
-      paragraph : "Nam sit amet pretium tortor. Praesent sagittis fermentum faucibus.",
-      title : "Host everything",
-      link:"/"
-  },{
-      img : "tool",
-      paragraph : "Nam sit amet pretium tortor. Praesent sagittis fermentum faucibus.",
-      title : "Fast & reliable",
-      link:"/"
-  }]
-  },{
-    type:"Media",
-    title:"Manage your tasks in the same place",
-    paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in sollicitudin quam, eu auctor elit. Integer id urna id tortor convallis semper sed sed odio. ",
-    image : client_image,
-  },{
-    type:"Sliders",
-    display: "arrows_split_center",
-    content: [
-      {
-        alternativeText:"illustration",
-        sliderImage,
-        title: "Build your own cloud",
-        p:"Nam sit amet pretium tortor. Praesent sagittis fermentum faucibus."
-      },
-      {
-        sliderImage,
-        alternativeText:"illustration",
-        title: "Build your own cloud",
-        p:"Nam sit amet pretium tortor. Praesent sagittis fermentum faucibus."
-      },
-      {
-        sliderImage,
-        title: "Build your own cloud",
-        p:"Nam sit amet pretium tortor. Praesent sagittis fermentum faucibus."
-      },
-      {
-        sliderImage,
-        alternativeText:"illustration",
-        title: "Build your own cloud",
-        p:"Nam sit amet pretium tortor. Praesent sagittis fermentum faucibus."
-      },
-      {
-        sliderImage,
-        alternativeText:"illustration",
-        title: "Build your own cloud",
-        p:"Nam sit amet pretium tortor. Praesent sagittis fermentum faucibus."
-      },
-      {
-        sliderImage,
-        alternativeText:"illustration",
-        title: "Build your own cloud",
-        p:"Nam sit amet pretium tortor. Praesent sagittis fermentum faucibus."
-      },
-      {
-        sliderImage,
-        alternativeText:"illustration",
-        title: "Build your own cloud",
-        p:"Nam sit amet pretium tortor. Praesent sagittis fermentum faucibus."
-      },
-    ]
-  },
-  {
-    type: "footer",
-    display: "column"
-  }];
+
 
 
 const IndexPage = ({data}) => {
 
-  // let PROJECT_ID = "h3asrjaf";
-  // let DATASET = "production";
-  // let QUERY = encodeURIComponent('*[_type == "header"]');
-  // // Compose the URL for your project's endpoint and add the query
-  // let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
+  const slices = []
+  slices.push(data.sanityLanding.header)
+  slices.push(...data.sanityLanding.contenu)
+  slices.push(data.sanityLanding.footer)
+  console.log(slices)
 
-  const slices = [
-    data.sanityHeader,
-    data.sanityHero,
-    data.sanityMedia,
-    data.sanityFooter,
-    data.sanitySlider,
-    data.sanitySteps
-  ]
-
-  return (
+return (
     <>
       <SliceMachine 
       Slices={slices}/>
-      {/* Slices={workArray}/> */}
     </>
   )
 }
@@ -138,174 +36,71 @@ export default IndexPage
 
 export const query = graphql`
 query MyQuery {
-  sanityHeader {
-    display
-    internal {
-      type
+
+  sanityLanding {
+    header {
+      display
+      _type
     }
-  }
-  
-  sanityHero {
-    internal {
-      type
-    }
-    display
-    title
-    secondaryParagraph
-    primaryParagraph
-    firstButton
-    seccondButton
-    image{
-      asset{
-        id
-        height
-        gatsbyImageData
-        gatsbyImage
-        filesize
-        filename
-        extension
-        description
-        assetId
-        _updatedAt
-        _type
-        _rev
-        _rawSource
-        _rawMetadata
+    contenu{
+      ... on SanityHero {
         _key
-        _id       id
-        height
-        gatsbyImageData
-        gatsbyImage
-        filesize
-        filename
-        extension
-        description
-        assetId
-        _updatedAt
         _type
-        _rev
-        _rawSource
-        _rawMetadata
-        _key
-        _id
+        display
+        firstButton
+        primaryParagraph
+        seccondButton
+        secondaryParagraph
+        title
+        altText
+        image{
+          asset{
+            publicUrl
+          }
+        }
       }
-    }
-  }
-
-
-
-  sanityMedia {
-    image{
-      asset{
-        id
-        height
-        gatsbyImageData
-        gatsbyImage
-        filesize
-        filename
-        extension
-        description
-        assetId
-        _updatedAt
+      ... on SanityMedia {
         _type
-        _rev
-        _rawSource
-        _rawMetadata
-        _key
-        _id       id
-        height
-        gatsbyImageData
-        gatsbyImage
-        filesize
-        filename
-        extension
-        description
-        assetId
-        _updatedAt
-        _type
-        _rev
-        _rawSource
-        _rawMetadata
-        _key
-        _id
+        title
+        primaryParagraph
+        altText
+        image{
+          asset{
+            publicUrl
+          }
+        }
       }
-    }
-    title
-    primaryParagraph
-    internal {
-      type
-    }
-  }
-  
-
-
-  sanityFooter {
-    display
-    internal {
-      type
-    }
-  }
-
-
-  sanitySlider {
-    display
-    slides {
-      title
-      paragraph
-      image{
-        asset{
-          id
-          height
-          gatsbyImageData
-          gatsbyImage
-          filesize
-          filename
-          extension
-          description
-          assetId
-          _updatedAt
-          _type
-          _rev
-          _rawSource
-          _rawMetadata
-          _key
-          _id       id
-          height
-          gatsbyImageData
-          gatsbyImage
-          filesize
-          filename
-          extension
-          description
-          assetId
-          _updatedAt
-          _type
-          _rev
-          _rawSource
-          _rawMetadata
-          _key
-          _id
+      ... on SanitySlider {
+        _type
+        display
+        name
+        slides{
+          title
+          paragraph
+          altText
+          image{
+            asset{
+              publicUrl
+            }
+          }
+        }
+      }
+      ... on SanitySteps {
+        _type
+        name
+        steps {
+          img
+          link
+          paragraph
+          title
         }
       }
     }
-    internal {
-      type
+    footer {
+      display
+      _type
     }
   }
-  sanitySteps {
-    steps {
-      name
-      paragraph
-      link
-      img
-    }
-
-    internal {
-      type
-    }
-  }
-
-
 
 }`;
 
